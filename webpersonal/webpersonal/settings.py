142ac9c2d351af09dde48e6c581a19e2588d400b
francoxr <franco.roca.pe@gmail.com>
2022-03-15 21:10:21 -0500

    add statics

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -119,12 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Media Files, url >> ruta publica donde django buscar los archivos media
 MEDIA_URL = '/media/'
 
 # ruta interna en donde se ubican los archivos media
-
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
